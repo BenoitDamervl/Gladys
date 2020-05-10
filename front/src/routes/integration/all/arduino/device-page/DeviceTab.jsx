60f4b635c4076d92d085728021dfbcea08ee6e37
billonalex <billonalex21@gmail.com>
2020-05-10 23:51:07 +0200

    debug

--- front/src/routes/integration/all/arduino/device-page/DeviceTab.jsx
+++ front/src/routes/integration/all/arduino/device-page/DeviceTab.jsx
@@ -63,7 +63,7 @@ const DeviceTab = ({ children, ...props }) => (
               <Text id="integration.arduino.device.arduinoNotConnected" />
             </p>
           )}
-          {props.arduinoDevices && props.arduinoDevices.length > 0 && props.devices.length === 0 && (
+          {props.arduinoDevices && props.arduinoDevices.length > 0 && !props.devices && (
             <div class="dimmer-content alert alert-info">
               <Text id="integration.arduino.device.noDevices" />
             </div>
